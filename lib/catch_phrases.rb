2a8bcee19482fed46c4534c42b248bd25d580ad6
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-28 02:41:51 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,4 @@
-# In the Mario Method, we must define the variable called phrase
-
-
-def mario
+def mario(phrase)
   phrase = "It's-a-me, Mario!"
   puts phrase
 end
@@ -9,7 +6,7 @@ end
 mario
 
 
-def toadstool
+def toadstool(status)
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
@@ -18,4 +15,18 @@ toadstool
 
 
 
-def link
+def link(new_phrase)
+  new_phrase = "It's Dangerous To Go Alone! Take This."
+  puts new_phrase
+end
+
+new_phrase
+
+def all_phrases
+  mario(phrase)
+  toadstool(status)
+  link(new_phrase)
+end
+
+all_phrases
+  
