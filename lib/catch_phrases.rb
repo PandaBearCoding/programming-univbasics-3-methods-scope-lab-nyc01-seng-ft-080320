cb160890d1a6640cb66099a02e229bc00ed4f538
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-28 02:42:55 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,32 +1,20 @@
-def mario(phrase)
+def mario
   phrase = "It's-a-me, Mario!"
   puts phrase
 end
 
-mario
-
-
-def toadstool(status)
+def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
 
-toadstool
-
-
-
-def link(new_phrase)
+def link
   new_phrase = "It's Dangerous To Go Alone! Take This."
   puts new_phrase
 end
 
-new_phrase
-
 def all_phrases
-  mario(phrase)
-  toadstool(status)
-  link(new_phrase)
+  all_phrases = mario(phrase) + toadstool(status) + link(new_phrase)
+  puts all_phrases
 end
-
-all_phrases
   
