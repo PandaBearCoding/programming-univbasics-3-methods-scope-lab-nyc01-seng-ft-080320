3c6a09c42bab936c55d6129bb21090fe5e3d4f6d
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-28 02:17:39 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,6 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+def mario (phrase)
+  phrase = "It's-a-me, Mario!"
+  if status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts phrase
 end
 
