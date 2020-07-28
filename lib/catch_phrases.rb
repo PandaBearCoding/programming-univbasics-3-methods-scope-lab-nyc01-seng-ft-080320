2180c6aea3c253a018a68883e8649722273f6934
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-28 02:19:55 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,10 @@
 def mario (phrase, status)
   if phrase = "It's-a-me, Mario!"
-  puts status = "Thank You Mario! But Our Princess Is In Another Castle!"
+   status = "Thank You Mario! But Our Princess Is In Another Castle!"
+  puts phrase
 end
 
 def toadstool(status)
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
-
